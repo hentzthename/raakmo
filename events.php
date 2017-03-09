7b0629862357a3340feea2acf52f7c55ff858c30
hentzthename <drewhentz@gmail.com>
2017-03-08 22:57:50 -0500

    Updated bio and events

--- events.php
+++ events.php
@@ -16,96 +16,15 @@
 			</thead>
 			<tbody>
 				<tr>
-					<td>April 16</td>
-					<td>Evil Olive</td>
-					<td>Chicago, IL</td>
-					<td>
-						<a href="https://www.facebook.com/events/1003754189695234/" target="_blank">
-							<button type="button" class="btn btn-info">More Info</button>
-						</a>
-					</td>
-				</tr>
-
-				<tr>
-					<td>April 26</td>
-					<td>Trap House</td>
-					<td>Chicago, IL</td>
-					<td>
-						<a href="https://www.facebook.com/events/100150033721638/" target="_blank">
-							<button type="button" class="btn btn-info">More Info</button>
-						</a>
-					</td>
-				</tr>
-				<tr>
-					<td>May 27</td>
-					<td>Volkan</td>
-					<td>Chicago, IL</td>
-					<td>
-						<a href="http://theticketrumba.com/tickets/4442" target="_blank">
-							<button type="button" class="btn btn-info">More Info</button>
-						</a>
-					</td>
-				</tr>
-				<tr>
-					<td>June 10</td>
+					<td>June 9, 2016</td>
 					<td>Spring Awakening</td>
 					<td>Chicago, IL</td>
 					<td>
-						<a href="https://www.facebook.com/springawakeningfest/" target="_blank">
+						<a href="http://www.springawakeningfestival.com/" target="_blank">
 							<button type="button" class="btn btn-info">More Info</button>
 						</a>
 					</td>
 				</tr>
-				<tr>
-					<td>July 8</td>
-					<td>Max Bar</td>
-					<td>Chicago, IL</td>
-					<td>
-						<a href="https://www.facebook.com/events/1734292563525254/" target="_blank">
-							<button type="button" class="btn btn-info">More Info</button>
-						</a>
-					</td>
-				</tr>
-				<tr>
-					<td>July 11</td>
-					<td>Evil Olive</td>
-					<td>Chicago, IL</td>
-					<td>
-						<a href="https://www.facebook.com/events/893964564082569/" target="_blank">
-							<button type="button" class="btn btn-info">More Info</button>
-						</a>
-					</td>
-				</tr>
-				<tr>
- +					<td>Aug 6</td>
- +					<td>Evil Olive</td>
- +					<td>Chicago, IL</td>
- +					<td>
- +						<a href="https://www.facebook.com/events/167572940321247/" target="_blank">
- +							<button type="button" class="btn btn-info">More Info</button>
- +						</a>
- +					</td>
- +				</tr>
- +				<tr>
- +					<td>Oct 4</td>
- +					<td>Trap House</td>
- +					<td>Chicago, IL</td>
- +					<td>
- +						<a href="https://www.facebook.com/events/205404299873189/" target="_blank">
- +							<button type="button" class="btn btn-info">More Info</button>
- +						</a>
- +					</td>
- +				</tr>
- +				<tr>
- +					<td>Oct 8</td>
- +					<td>Club 18</td>
- +					<td>Chicago, IL</td>
- +					<td>
- +						<a href="https://www.facebook.com/events/313136135712672/" target="_blank">
- +							<button type="button" class="btn btn-info">More Info</button>
- +						</a>
- +					</td>
- +				</tr>
 			</tbody>
 		</table>
 	</div>
