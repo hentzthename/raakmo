cc72f24b48e027b37acd30d32285f9321544772c
hentzthename <drewhentz@gmail.com>
2017-07-27 23:24:55 -0400

    Update events.php

--- events.php
+++ events.php
@@ -15,8 +15,19 @@
 				</tr>
 			</thead>
 			<tbody>
+				
 				<tr>
-					<td>June 9, 2016</td>
+					<td>August 5, 2017</td>
+					<td>Lolla After Hours</td>
+					<td>Chicago, IL</td>
+					<td>
+						<a href="https://www.facebook.com/events/1434708349952034/?acontext=%7B%22ref%22%3A%2229%22%2C%22ref_notif_type%22%3A%22plan_user_invited%22%2C%22action_history%22%3A%22null%22%7D&notif_t=plan_user_invited&notif_id=1500570702790483" target="_blank">
+							<button type="button" class="btn btn-info">More Info</button>
+						</a>
+					</td>
+				</tr>
+				<tr>
+					<td>June 9, 2017</td>
 					<td>Spring Awakening</td>
 					<td>Chicago, IL</td>
 					<td>
